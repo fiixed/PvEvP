0724ef6ced2cc60126a2af7de3494e0d00641165
Andrew Bell <fiixed@gmail.com>
2017-04-02 13:18:11 -0500

    feat:  Enemy animation

---
+++ Assets/RobotMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RobotMovement : MonoBehaviour {
+
+	Transform player;
+	//PlayerHealth playerHealth;
+	//RobotHealth robotHealth;
+	NavMeshAgent nav;
+
+	private void Awake() {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		nav = GetComponent<NavMeshAgent>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination(player.position);
+	}
+}
